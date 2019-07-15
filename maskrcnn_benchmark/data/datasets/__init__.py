b9f571ca48c31d9a5c64d1e63c30c976621ea170
Sulabh Shrestha <sulabh.shr@gmail.com>
2019-07-15 18:12:33 -0400

    Add active vision coco-like class to the list of available classes in init

--- maskrcnn_benchmark/data/datasets/__init__.py
+++ maskrcnn_benchmark/data/datasets/__init__.py
@@ -2,5 +2,7 @@
 from .coco import COCODataset
 from .voc import PascalVOCDataset
 from .concat_dataset import ConcatDataset
+from .active_vision_coco import ActiveVisionCOCODataset
 
-__all__ = ["COCODataset", "ConcatDataset", "PascalVOCDataset"]
+__all__ = ["COCODataset", "ConcatDataset", "PascalVOCDataset",
+           "ActiveVisionCOCODataset"]
